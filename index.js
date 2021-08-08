dadf00640d5bfcb2cfa061ddfd08f8f1482e5bde
AppleJaxxZ <smackanalex@gmail.com>
2021-08-08 19:05:04 -0400

    Main scraper has been made, added 2captcha, puppeteer, request, promise-poller, and dotenv packages.  Connected scraper to 2captcha recaptcha solver api, all form data, and receive a snapshop of the data wanteed

---
+++ index.js
@@ -0,0 +1,124 @@
+const puppeteer = require("puppeteer");
+const request = require("request-promise-native").defaults({ Jar: true });
+const poll = require("promise-poller").default;
+require("dotenv").config();
+
+const config = {
+  sitekey: process.env.SITEKEY,
+  pageurl: process.env.PAGEURL,
+  apiKey: process.env.APIKEY,
+  apiSubmitUrl: "http://2captcha.com/in.php",
+  apiRetrieveUrl: "http://2captcha.com/res.php",
+};
+
+const getPIN = function () {
+  return "2520228";
+};
+
+const getDOB = function () {
+  return "09/10/1987";
+};
+
+const chromeOptions = {
+  executablePath: "/Program Files/Google/Chrome/Application/chrome.exe",
+  headless: true,
+  slowMo: 60,
+  defaultViewport: null,
+};
+
+async function main() {
+  const browser = await puppeteer.launch(chromeOptions);
+  const page = await browser.newPage();
+
+  console.log(`Navigating to ${config.pageurl}`);
+  await page.goto(config.pageurl);
+  try {
+    const requestId = await initiateCaptchaRequest(config.apiKey);
+
+    const pin = getPIN();
+    console.log(`Typing PIN ${pin}`);
+    await page.type("#PIN", pin);
+
+    const dob = getDOB();
+    console.log(`Typing DOB ${dob}`);
+    const input = await page.$("#DOB");
+    await input.click({ clickCount: 3 });
+    await input.type(dob);
+
+    const response = await pollForRequestResults(config.apiKey, requestId);
+
+    console.log(`Entering recaptcha response ${response}`);
+    await page.evaluate(
+      `document.getElementById("g-recaptcha-response").innerHTML="${response}";`
+    );
+
+    console.log(`Submitting....`);
+    page.click("#Submit");
+  } catch (error) {
+    console.error(error);
+  }
+
+  await page.waitForSelector(
+    "body > div.container.body-content > div:nth-child(1) > div:nth-child(2) > p"
+  );
+  const image = await page.$(
+    "body > div.container.body-content > div:nth-child(1) > div:nth-child(2) > p"
+  );
+  await image.screenshot({
+    path: "testResults.png",
+  });
+  await page.close(); // Close the website
+  await browser.close(); //close browser
+}
+main();
+
+async function initiateCaptchaRequest(apiKey) {
+  const formData = {
+    key: apiKey,
+    method: "userrecaptcha",
+    googlekey: config.sitekey,
+    json: 1,
+    pageurl: config.pageurl,
+  };
+
+  console.log(`Submitting recaptcha request to 2captcha for ${config.pageurl}`);
+  const response = await request.post(config.apiSubmitUrl, { form: formData });
+  console.log(response);
+  return JSON.parse(response).request;
+}
+
+async function pollForRequestResults(
+  key,
+  id,
+  retries = 90,
+  interval = 5000,
+  delay = 1500
+) {
+  console.log(`Waiting for ${delay} milliseconds....`);
+  await timeout(delay);
+  return poll({
+    taskFn: requestCaptchaResults(key, id),
+    interval,
+    retries,
+  });
+}
+
+function requestCaptchaResults(apiKey, requestId) {
+  const url = `${config.apiRetrieveUrl}?key=${apiKey}&action=get&id=${requestId}&json=1`;
+  console.log(url);
+  return async function () {
+    return new Promise(async function (resolve, reject) {
+      console.log(`Polling for response...`);
+      const rawResponse = await request.get(url);
+      console.log(rawResponse);
+      const resp = JSON.parse(rawResponse);
+      console.log(resp);
+      if (resp.status === 0) return reject(resp.request);
+      console.log("Response received");
+      console.log(resp);
+      resolve(resp.request);
+    });
+  };
+}
+
+const timeout = (ms) => new Promise((res) => setTimeout(res, ms));
