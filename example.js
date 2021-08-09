da25772ff678cb121176863194906c62959db5f3
AppleJaxxZ <smackanalex@gmail.com>
2021-08-09 15:29:43 -0400

    updated googles api keys path and added to gitignore

--- example.js
+++ example.js
@@ -3,21 +3,27 @@ const fs = require("fs");
 require("dotenv").config();
 
 async function getImageText() {
-  // Creates a client
-  const client = new vision.ImageAnnotatorClient();
-  console.log(`Looking for text in image`);
-  // Performs label detection on the image file
-  const [result] = await client.textDetection("./testResults.png");
-  const [annotation] = result.textAnnotations;
-  console.log(annotation);
-  const text = annotation ? annotation.description : "";
-  console.log("Extracted text from image:", text);
-  const path = "./testResults.png";
   try {
+    // Creates a client
+    const client = new vision.ImageAnnotatorClient();
+    console.log(`Looking for text in image`);
+    // Performs label detection on the image file
+    const [result] = await client.textDetection("./testResults.png");
+    const [annotation] = result.textAnnotations;
+    console.log(annotation);
+    const text = annotation ? annotation.description : "";
+    console.log("Extracted text from image:", text);
+  } catch (error) {
+    console.log("There is no text to display, please re-try again later.");
+  }
+  try {
+    const path = "./testResults.png";
     fs.unlinkSync(path);
     console.log("File removed:", path);
   } catch (err) {
-    console.error(err);
+    console.log(
+      "There is no file to delete..The file has already been deleted or was not downloaded.  Check the file name and location."
+    );
   }
 }
 getImageText();
