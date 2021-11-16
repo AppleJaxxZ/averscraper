ce9e67a9680cfe2b1f1d6b5acff4f66f4453f7b3
AppleJaxxZ <smackanalex@gmail.com>
2021-11-16 09:59:24 -0500

    Updated .env config names

--- scraper.js
+++ scraper.js
@@ -110,16 +110,16 @@ const scraper = async (pinNum, dateOfB) => {
   async function getImageText() {
     // Creates a client
     const client = new vision.ImageAnnotatorClient({
-      type: process.env.TYPE,
-      project_id: process.env.PROJECT_ID,
-      private_key_id: process.env.PRIVATE_KEY_ID,
-      private_key: process.env.PRIVATE_KEY,
-      client_email: process.env.CLIENT_EMAIL,
-      client_id: process.env.CLIENT_ID,
-      auth_uri: process.env.AUTH_URI,
-      token_uri: process.env.TOKEN_URI,
-      auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
-      client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+      type: process.env.GOOGLE_TYPE,
+      project_id: process.env.GOOGLE_PROJECT_ID,
+      private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+      private_key: process.env.GOOGLE_PRIVATE_KEY,
+      client_email: process.env.GOOGLE_CLIENT_EMAIL,
+      client_id: process.env.GOOGLE_CLIENT_ID,
+      auth_uri: process.env.GOOGLE_AUTH_URI,
+      token_uri: process.env.GOOGLE_TOKEN_URI,
+      auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_CERT,
+      client_x509_cert_url: process.env.GOOGLE_CLIENT_CERT,
       // keyFilename: "./googlevisionapikeys.json",
     });
     console.log(`Looking for text in image`);
