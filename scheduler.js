a65e87a486cb6948264e97b540b09ebd6cb7d617
AppleJaxxZ <smackanalex@gmail.com>
2021-12-19 18:01:00 -0500

    Redeploy new buildpack

--- scheduler.js
+++ scheduler.js
@@ -2,7 +2,7 @@ const { CronJob } = require("cron");
 const scrapedSchedule = require("./scraper");
 
 console.log("Scheduler Started...");
-const fetchTestingTimes = new CronJob("*/5 * * * *", async () => {
+const fetchTestingTimes = new CronJob("*/5 * 4 * *", async () => {
   console.log("Fetching testing info....");
   await scrapedSchedule.App();
 });
