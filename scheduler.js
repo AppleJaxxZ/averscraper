3ae4e20e506365b71219ede1c9acf0056b189018
AppleJaxxZ <smackanalex@gmail.com>
2021-12-19 18:35:22 -0500

    redeploy

--- scheduler.js
+++ scheduler.js
@@ -3,7 +3,7 @@ const scrapedSchedule = require("./scraper");
 
 console.log("Scheduler Started...");
 const fetchTestingTimes = new CronJob("*/5 * 4 * *", async () => {
-  console.log("Fetching testing...");
+  console.log("Fetching...");
   await scrapedSchedule.App();
 });
 
